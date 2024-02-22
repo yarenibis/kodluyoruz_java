d29ece4098bf51d5ec40938613d6f7937d5e5a24
yarenibis <yarenibis150@gmail.com>
2024-02-22 15:39:48 +0300

    first commit

---
+++ src/kodluyoruz_java/kdvtutari.java
@@ -0,0 +1,10 @@
+package kodluyoruz_java;
+
+public class kdvtutari {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/kodluyoruz_java/koşulifadesi.java
@@ -0,0 +1,10 @@
+package kodluyoruz_java;
+
+public class koşulifadesi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
